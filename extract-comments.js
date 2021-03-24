2f0d3537147ad63d99c53480df1ff6ab2366a5ef
stadibo <jesperleonard@gmail.com>
2021-03-24 13:11:33 +0200

    Save replies along with main comment - The replies are loaded only when manually opened - If replies not loaded the replies field is null

--- extract-comments.js
+++ extract-comments.js
@@ -8,40 +8,73 @@ const runFiltering = async () => {
   };
 
   const extractYoutubeComments = () => {
-    console.log('EXTRACTING COMMENTS')
+    const getCommentData = (item) => {
+      const mainWrapper = item.querySelector("#body > #main");
+      const author = mainWrapper
+        .querySelector("#author-text > span")
+        .textContent.trim();
+      const commentText = mainWrapper
+        .querySelector("#content > #content-text")
+        .textContent.trim();
+
+      return { author: author, content: commentText };
+    };
+    console.log("EXTRACTING COMMENTS");
 
     return new Promise((resolve, reject) => {
       try {
-
         const commentsContainer = document.querySelector("#comments");
         const contents = commentsContainer.querySelector("#contents");
-        const commentRenderers = contents.querySelectorAll("#comment");
-        const commentEntries = Array.from(commentRenderers, (item) => {
-          const mainWrapper = item.querySelector("#body > #main");
-          const author = mainWrapper
-          .querySelector("#author-text > span")
-          .textContent.trim();
-          const commentText = mainWrapper
-          .querySelector("#content > #content-text")
-          .textContent.trim();
-          
+        // const commentRenderers = contents.querySelectorAll("#comment");
+        const commentRenderers = contents.querySelectorAll(
+          "ytd-comment-thread-renderer"
+        );
+        const commentEntries = Array.from(commentRenderers, (renderer) => {
+          // GET main comment in thread
+          const comment = renderer.querySelector("#comment");
+          const commentData = comment ? getCommentData(comment) : null;
+
+          // GET replies
+          const repliesWrapper = renderer.querySelector("#loaded-replies");
+          let replies = null;
+
+          if (repliesWrapper) {
+            const replyElements = repliesWrapper.querySelectorAll(
+              "ytd-comment-renderer"
+            );
+            replies =
+              replyElements &&
+              Array.from(replyElements, (element) => {
+                const replyData = element ? getCommentData(element) : {};
+                return {
+                  data: { ...replyData },
+                  element: element
+                };
+              });
+          }
+
           // DEBUG
           // console.log(author);
           // console.log(commentText);
           // console.log(item);
-          
-          return { data: { author: author, content: commentText }, element: item };
+
+          return {
+            data: {
+              ...commentData,
+              replies: replies,
+            },
+            element: renderer,
+          };
         });
         resolve(commentEntries);
       } catch (err) {
-        reject(err)
+        reject(err);
       }
-    })
-
+    });
   };
 
   const getLabelsForComments = (comments) => {
-    console.log('FETCHING LABELS')
+    console.log("FETCHING LABELS");
 
     return Promise.all(
       comments.map((item) => {
@@ -51,31 +84,30 @@ const runFiltering = async () => {
             return { ...item, ...data };
           })
           .catch((err) => {
-            console.log('LABEL DATA ERROR', err)
+            console.log("LABEL DATA ERROR", err);
             return { ...item, label: "unknown" };
           });
         return response;
       })
-    ).catch(err => {
-      console.log('Ooooh dammmmm', err)
+    ).catch((err) => {
+      console.log("Ooooh dammmmm", err);
     });
-  }
+  };
 
   const hideComments = () => {
-    console.log('HIDING COMMENTS')
+    console.log("HIDING COMMENTS");
     // Do some hiding business here
-  }
+  };
 
   const comments = await extractYoutubeComments();
   console.log(comments);
 
   // GET LABELS FROM API
-  const comments_with_labels = await getLabelsForComments(comments)
-  console.log(comments_with_labels)
+  const comments_with_labels = await getLabelsForComments(comments);
+  console.log(comments_with_labels);
 
   // HIDE
-  hideComments(comments_with_labels )
-
+  hideComments(comments_with_labels);
 };
 
 changeColor.addEventListener("click", async () => {
