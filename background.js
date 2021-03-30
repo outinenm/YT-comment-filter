a039f37fddf8d9aa727892bfb5df8001e32346c0
stadibo <jesperleonard@gmail.com>
2021-03-30 11:40:35 +0300

    Add clarification for logging

--- background.js
+++ background.js
@@ -5,57 +5,58 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log("YT comment color set to %cgreen", `color: ${color}`);
 });
 
-// const testScript = () => {
-//   // Object.keys(window).forEach((key) => {
-//   //   console.log(key)
-//   //   if (/^on/.test(key)) {
-//   //     window.addEventListener('animationiteration', (event) => {
-//   //       console.log('The event we want')
-//   //       // console.log('path',event.path)
-//   //       console.log('e',event);
-//   //     });
-//   //     // window.addEventListener(key.slice(2), (event) => {
-//   //     //   console.log(event);
-//   //     // });
-//   //   }
-//   // });
+const testScript = () => {
+  // Object.keys(window).forEach((key) => {
+  //   console.log(key)
+  //   if (/^on/.test(key)) {
+  //     window.addEventListener('animationiteration', (event) => {
+  //       console.log('The event we want')
+  //       // console.log('path',event.path)
+  //       console.log('e',event);
+  //     });
+  //     // window.addEventListener(key.slice(2), (event) => {
+  //     //   console.log(event);
+  //     // });
+  //   }
+  // });
 
-//   const body = document.querySelector("body");
-//   console.log(body);
-//   const commentsEl = body.querySelector("#comments");
-//   // commentsEl.addEventListener("change", (event) => {
-//   //   console.log("comment contents", event);
-//   // });
-//   console.log(commentsEl);
-  
-//   const obs = (muts) => {
-//     console.log("MUTATION HAPPENED");
-//     console.log(muts);
-    
-//     for (const m of muts) {
-//       for (const n of m.addedNodes) {
-//         console.log(n.id);
-//         console.log(n);
-//         if (n.id === "sections") {
-//           const node = n.querySelector("#contents");
-//           console.log("Da shit:", node);
-//           console.log(node.childNodes)
-//           const testObs = (muts) => {
-//             console.log(muts)
-//           }
-          
-//           // MAKE OBSERVER THAT CALLS FILTER FUNCTION
-          
-//           const o2 = new MutationObserver(testObs);
-//           o2.observe(node, { childList: true });
-//         }
-//       }
-//     }
-//   };
+  const body = document.querySelector("body");
+  console.log(body);
+  const commentsEl = body.querySelector("#comments");
+  // commentsEl.addEventListener("change", (event) => {
+  //   console.log("comment contents", event);
+  // });
+  console.log(commentsEl);
 
-//   const observer = new MutationObserver(obs);
-//   observer.observe(commentsEl, { childList: true });
-// };
+  const obs = (muts) => {
+    console.log("MUTATION HAPPENED");
+    console.log('Mutations', muts);
+
+    for (const m of muts) {
+      for (const n of m.addedNodes) {
+        console.log("Sub node", n.id);
+        console.log("Sub node", n);
+        if (n.id === "sections") {
+          const node = n.querySelector("#contents");
+          console.log("Sections node", node);
+          console.log("Sections node", node.childNodes);
+          const testObs = (muts) => {
+            console.log('Mutations on section node', muts);
+          };
+
+          // MAKE OBSERVER THAT CALLS FILTER FUNCTION
+
+          const o2 = new MutationObserver(testObs);
+          o2.observe(node, { childList: true });
+          break;
+        }
+      }
+    }
+  };
+
+  const observer = new MutationObserver(obs);
+  observer.observe(commentsEl, { childList: true });
+};
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   console.log(tabId);
@@ -71,17 +72,15 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
 
       if (url && url.includes("youtube.com/watch")) {
         console.log("Add listener for comments", tabs[0].id);
-        
+
         // UNCOMMENT TO CALL EXTRACTION SCRIPT WHEN HITTING YOUTUBE WATCH PAGE
-        
-        // chrome.scripting.executeScript({
-        //   target: { tabId: tabs[0].id },
-        //   function: testScript,
-        // });
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          function: testScript,
+        });
       }
 
       // use `url` here inside the callback because it's asynchronous!
     });
-    // do your things
   }
 });
